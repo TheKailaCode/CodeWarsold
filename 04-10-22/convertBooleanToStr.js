78bde1889c21f18616c62e31a8d6186184f7ba6e
Kaila Kelly <100878769+TheKailaCode@users.noreply.github.com>
2022-04-10 15:53:16 -0400

    Add files via upload
    completed codewars challenges for the day

---
+++ 04-10-22/convertBooleanToStr.js
@@ -0,0 +1,10 @@
+//INSTRUCTIONS
+
+//Implement a function which convert the given boolean value into its string representation.
+
+function booleanToString(b) {
+    //your code here
+    return b.toString()
+}
+
+  //use the toString method to convert a boolean to a string
---
+++ 04-10-22/convertStringToNum.js
@@ -0,0 +1,11 @@
+//INSTRUCTIONS
+
+//We need a function that can transform a string into a number. What ways of achieving this do you know?
+
+var stringToNumber = function (str) {
+    // put your code here
+
+    return Number(str);
+}
+
+  //number method used to convert string to a number
