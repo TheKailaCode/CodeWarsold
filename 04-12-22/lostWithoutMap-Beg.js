0eb51db79d0c17a1802d6a34ca905faf8a1c6781
Kaila Kelly <100878769+TheKailaCode@users.noreply.github.com>
2022-04-12 22:09:13 -0400

    Add files via upload
    completed codewars challenge

---
+++ 04-12-22/lostWithoutMap-Beg.js
@@ -0,0 +1,13 @@
+//Instructions
+
+//Given an array of integers, return a new array with each value doubled.
+
+function maps(x) {
+
+    let newArr = x.map(x => x * 2)
+    return newArr
+
+}
+  //use the map function to grab the element in the array
+  //multiply the element times 2
+  //map function returns a new array with updated element values doubled
