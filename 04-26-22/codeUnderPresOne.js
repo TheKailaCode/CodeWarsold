425215796a54c2b1d452f64e11be8885cf81d23d
Kaila Kelly <100878769+TheKailaCode@users.noreply.github.com>
2022-04-26 07:58:00 -0400

    Add files via upload
    completed daily codewar challenges

---
+++ 04-26-22/codeUnderPresOne.js
@@ -0,0 +1,10 @@
+// Instructions
+
+// Code as fast as you can! You need to double the integer and return it.
+
+function doubleInteger(i) {
+    // i will be an integer. Double it and return it.
+    let doubled = i *= 2
+
+    return doubled;
+}
