12217fd11a8a33b38d375c33fa41214124129d75
Kaila Kelly <100878769+TheKailaCode@users.noreply.github.com>
2022-05-04 16:14:46 -0400

    Add files via upload
    completed daily codewars challenge

---
+++ 05-04-22/binToDecimal.js
@@ -0,0 +1,9 @@
+// Instructions
+
+// Complete the function which converts a binary number (given as a string) to a decimal number.
+
+function binToDec(bin) {
+    // ...
+    let decimalNum = parseInt(bin, 2)
+    return decimalNum
+}
