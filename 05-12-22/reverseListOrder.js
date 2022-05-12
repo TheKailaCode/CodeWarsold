29be033d0b7846a7b91e70e8d5f3dcf2a00eed35
Kaila Kelly <100878769+TheKailaCode@users.noreply.github.com>
2022-05-12 13:45:41 -0400

    Add files via upload
    completed daily code challenge

---
+++ 05-12-22/reverseListOrder.js
@@ -0,0 +1,8 @@
+// Instructions
+
+// In this kata you will create a function that takes in a list and returns a list with the reverse order.
+
+function reverseList(list) {
+    let newList = list.reverse()
+    return newList
+}
